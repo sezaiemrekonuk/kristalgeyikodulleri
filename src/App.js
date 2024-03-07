6b5ff8d78b7b132510aca03db30e0c6017232f92
sezaiemrekonuk <sezaikonuk@hacettepe.edu.tr>
2024-03-07 16:13:46 +0300

    usonmedia component

--- src/App.js
+++ src/App.js
@@ -5,14 +5,15 @@ import bgImage from './images/background.png';
 import Navbar from "./Components/Navbar";
 import Socials from "./Components/Socials";
 import Loader from "./Loaders/Loader";
-import {SliderEngineering} from "./Pages/SliderEngineering";
-import SliderManagement from "./Pages/SliderManagement";
-import SliderArtCulture from "./Pages/SliderArtCulture";
 
 const Main = React.lazy(() => import('./Pages/Main'));
 const Announcements = React.lazy(() => import('./Pages/Announcements'));
 const About = React.lazy(() => import('./Pages/About'));
 const Sponsors = React.lazy(() => import('./Pages/Sponsors'));
+const SliderEngineering = React.lazy(() => import('./Pages/SliderEngineering'))
+const SliderManagement = React.lazy(() => import('./Pages/SliderManagement'))
+const SliderArtCulture = React.lazy(() => import('./Pages/SliderArtCulture'))
+const UsOnMedia = React.lazy(() => import('./Pages/UsOnMedia'))
 
 export default function App() {
     return (
@@ -21,32 +22,36 @@ export default function App() {
             <Socials/>
             <img src={bgImage} alt=""
                  className='min-w-[1920px] w-dvw h-dvh scale-[1.02] fixed -z-10 tranlate-x-0.5 translate-y-0.5 top-0.5 left-0.5'/>
-            <Suspense fallback={<Loader />}>
+            <Suspense fallback={<Loader/>}>
                 <Main name="ana"/>
             </Suspense>
 
-            <Suspense fallback={<Loader />}>
+            <Suspense fallback={<Loader/>}>
                 <Announcements name="duyurular"/>
             </Suspense>
 
-            <Suspense fallback={<Loader />}>
+            <Suspense fallback={<Loader/>}>
                 <About name="hakkinda"/>
             </Suspense>
 
-            <Suspense fallback={<Loader />}>
+            <Suspense fallback={<Loader/>}>
                 <Sponsors name="sponsorlar"/>
             </Suspense>
 
-            <Suspense fallback={<Loader />}>
+            <Suspense fallback={<Loader/>}>
                 <SliderEngineering name="kgo23"/>
             </Suspense>
 
-            <Suspense fallback={<Loader />}>
+            <Suspense fallback={<Loader/>}>
                 <SliderManagement name="kgo23"/>
             </Suspense>
 
-            <Suspense fallback={<Loader />}>
+            <Suspense fallback={<Loader/>}>
                 <SliderArtCulture name="kgo23"/>
+            </Suspense>
+
+            <Suspense fallback={<Loader/>}>
+                <UsOnMedia name={"medyadabiz"}/>
             </Suspense>
         </div>
     )
--- src/Pages/UsOnMedia.jsx
+++ src/Pages/UsOnMedia.jsx
@@ -1,14 +1,13 @@
-import CustomSlider from "../Components/Slider/Slider";
 import BigHeader from "../Components/Headers/BigHeader";
+import CustomSlider from "../Components/Slider/Slider";
 
-const SliderEngineering = (props) => {
+export const UsOnMedia = (props) => {
     return (
         <div id={props.name} className={"flex justify-center flex-col min-h-dvh pt-20 md:mx-28 mx-10"}>
             <div className="mx-auto mb-8 flex flex-col items-center justify-center text-center">
                 <BigHeader>
-                    KGÖ'24
+                    Medyada Biz
                 </BigHeader>
-                <h2 className={"sponsor-title text-4xl mb-4"}>Muhendislik Kazananlari</h2>
             </div>
 
             <CustomSlider/>
@@ -16,4 +15,4 @@ const SliderEngineering = (props) => {
     )
 }
 
-export default SliderEngineering;
+export default UsOnMedia;
